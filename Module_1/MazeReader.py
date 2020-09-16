65ffe934262f37aa50da5e325fd32ea66ebf2e5f
JFlash30 <jasontsmith35@gmail.com>
2020-09-16 16:15:33 -0500

    toString() method
    Chnges 2D enum back to 2D string

--- Module_1/MazeReader.py
+++ Module_1/MazeReader.py
@@ -5,14 +5,31 @@ class Square(Enum):
 	OPEN = 2
 	START = 3
 	FINISH = 4
-
+    
 class MazeReader:
 	def __init__():
-
-
-	def toString():
-
-
+        
+        
+	def toString(array):
+        az = []
+        for i in range(r):
+            az.append([])
+            for j in range(c):
+                az[-1].append("")
+        
+        for i in range(r):
+                if (array[i][j]==Square.WALL):
+                    az[i][j] = '#'
+                elif (array[i][j]==Square.OPEN):
+                    az[i][j] = '.'
+                elif (array[i][j]==Square.START):
+                    az[i][j] = 'o'
+                elif (array[i][j]==Square.FINISH):
+                    az[i][j] = '*'
+                else:
+                    raise ValueError
+        return az
+        
 	def fromChar(ch):
 		if (ch == '#'):
 			return Square.WALL
