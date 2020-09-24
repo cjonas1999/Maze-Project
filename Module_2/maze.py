6939a26f32fccd2fda0d1c3f60ec40907aa7bd3c
Casey Jonas <cjonas1999@gmail.com>
2020-09-23 21:17:11 -0500

    Added solve function
    Also included main.py to test

--- Module_2/maze.py
+++ Module_2/maze.py
@@ -23,7 +23,6 @@ class MazeReader:
 			self.rows += 1
 		
 		self.cols = len(self.maze[0])
-		print(self.maze)
 
 		f.close()
 
@@ -64,3 +63,34 @@ class MazeReader:
 class MazeSolver:
 	def __init__(self, mazereader: MazeReader):
 		self.mazereader = mazereader
+	
+	def solve(self):
+		#Mark all the vertices as not visited
+		visited = [[False for i in range(self.mazereader.cols)] for j in range(self.mazereader.rows)]
+		queue = []
+
+		#Find start square
+		for i in range(self.mazereader.rows):
+			for j in range(self.mazereader.cols):
+				if self.mazereader.maze[i][j] == Square.START:
+					queue.append([i, j])
+					visited[i][j] = True
+					break
+
+		while queue:
+			s = queue.pop(0)
+
+			for offset in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+				newsquare = [offset[0] + s[0], offset[1] + s[1]]
+
+				if(newsquare[0] < 0 and newsquare[0] >= self.mazereader.rows and newsquare[1] < 0 and newsquare[1] >= self.mazereader.cols):#checking that the square is valid to visit
+					continue
+
+				if(not visited[newsquare[0]][newsquare[1]] and self.mazereader.maze[newsquare[0]][newsquare[1]] != Square.WALL):
+						if self.mazereader.maze[newsquare[0]][newsquare[1]] == Square.FINISH:
+							return True
+						else:
+							queue.append(newsquare)
+							visited[newsquare[0]][newsquare[1]] = True
+
+		return False
